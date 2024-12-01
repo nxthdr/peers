a8ccba4d8aa94fd3d225f16058f95d4f443a1b0b
matthieugouel <matthieu.gouel@protonmail.com>
2024-12-01 16:55:09 +0100

    basic filtering

--- README.md
+++ README.md
@@ -1,39 +1,8 @@
 # peers
 
-This template should help get you started developing with Vue 3 in Vite.
+> [!WARNING]
+> Peers is currently in early-stage development.
 
-## Recommended IDE Setup
+Simple web page displaying the list of AS215011 peers based on BMP data.
 
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
-
-## Type Support for `.vue` Imports in TS
-
-TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vite.dev/config/).
-
-## Project Setup
-
-```sh
-npm install
-```
-
-### Compile and Hot-Reload for Development
-
-```sh
-npm run dev
-```
-
-### Type-Check, Compile and Minify for Production
-
-```sh
-npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
-```
+The page executes a SQL query through chproxy to retrieve a distinct list of ASes that appear first in the path the updates received within the past 24 hours.
--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,12 @@
   <main>
     <h1>Peers</h1>
     <p>Number of peers: {{ peers.length }}</p>
+    <input v-model="filterInput" placeholder="Filter peers" />
+    <p><br /></p>
     <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">ASN</th>
-      </tr>
-    </thead>
     <tbody>
-      <tr v-for="peer in peers">
-        <td>{{peer}}</td>
+      <tr v-for="peer in filteredPeers">
+        <td>{{ peer }}</td>
       </tr>
     </tbody>
   </table>
@@ -21,12 +18,27 @@
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue'
+  import { ref, computed } from 'vue'
   const peers = ref<string[]>([]);
+  const filterInput = ref<string>('');
+
   fetch('https://clickhouse.nxthdr.dev?user=read&password=read', {
         method: 'POST',
-        body: 'SELECT DISTINCT path[1] AS peer FROM nxthdr.bgp_updates WHERE peer != 0 ORDER BY peer FORMAT Json',
+        body: `
+          SELECT DISTINCT path[1] AS peer
+          FROM nxthdr.bgp_updates
+          WHERE peer != 0 AND timestamp >= subtractHours(now(), 24)
+          ORDER BY peer
+          FORMAT Json
+        `,
       })
       .then(response => response.json())
       .then(response => { response.data.forEach((peer: { peer: string }) => {  peers.value.push(peer.peer) }) });
+
+  const filteredPeers = computed(() => {
+    return peers.value.filter((peer) => {
+      if (!filterInput.value) return true;
+      return peer == filterInput.value;
+    } )
+  })
 </script>
